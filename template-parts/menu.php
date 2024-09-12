55cda22670259b733d729d2ede91afd65f0a28db
xarop <ajl@xarop.com>
2024-09-12 21:36:40 +0200

    hamburger fix

--- template-parts/menu.php
+++ template-parts/menu.php
@@ -27,13 +27,14 @@ $social = get_field('social', 'option');
         <?php endif; ?>
 
 
-        <button aria-label="Toggle navigation" id="primary-menu-toggle" class="hamburger">
+        <button aria-label="Toggle navigation" id="primary-menu-toggle" class="hamburger lg:hidden">
             <svg viewBox="0 0 20 20" class="inline-block w-6 h-6" version="1.1"
                 xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
                 <g stroke="none" stroke-width="1" fill="currentColor" fill-rule="evenodd">
                     <g id="icon-shape">
-                        <path d="M0,3 L20,3 L20,5 L0,5 L0,3 Z M0,9 L20,9 L20,11 L0,11 L0,9 Z M0,15 L20,15 L20,17 L0,17 L0,15 Z"
-                            id="Combined-Shape"></path>
+                        <path d="M0,3 L20,3 L20,5 L0,5 L0,3 Z"></path>
+                        <path d="M0,9 L20,9 L20,11 L0,11 L0,9 Z"></path>
+                        <path d="M0,15 L20,15 L20,17 L0,17 L0,15 Z"></path>
                     </g>
                 </g>
             </svg>
