3a73281eb6e356321a4e2f964e5af282c30ba942
xarop <ajl@xarop.com>
2024-09-13 09:27:14 +0200

    production home

--- footer.php
+++ footer.php
@@ -4,21 +4,48 @@
 
 <?php do_action('tailpress_content_after'); ?>
 
-<footer id="colophon" class="site-footer py-12" role="contentinfo">
+<footer id="colophon" class="site-footer container" role="contentinfo">
 	<?php do_action('tailpress_footer'); ?>
 
-	<div class="container lg:flex lg:justify-between lg:items-center">
+	<?php if (have_rows('contact', 'option')):  ?>
+		<ul class="flex w-full justify-between bg-black text-white p-10">
+			<?php
+			while (have_rows('contact', 'option')): the_row();
+				$name = get_sub_field('name');
+				$email = get_sub_field('email');
+				$tel = get_sub_field('tel');
+			?>
+				<li class="flex items-center gap-2">
+					<span><?php echo esc_attr($name); ?></span>
+					<a href="mailto:<?php echo esc_url($email); ?>" target="_blank" title="<?php echo esc_attr($email); ?>"><?php echo esc_attr($email); ?></a>
+					<a href="tel:<?php echo esc_url($tel); ?>" target="_blank" title="<?php echo esc_attr($tel); ?>"><?php echo esc_attr($tel); ?></a>
+				</li>
+			<?php endwhile; ?>
+		</ul>
+	<?php endif; ?>
+
+	<?php if (have_rows('banners', 'option')):  ?>
+		<section class="p-10 flex flex-wrap justify-center gap-10">
+			<?php
+			while (have_rows('banners', 'option')): the_row();
+				$name = get_sub_field('url');
+				$image = get_sub_field('image');
+			?>
+
+				<a href="<?php echo esc_url($url); ?>" target="_blank" nofollow>
+					<img src="<?php echo $image; ?>">
+				</a>
 
 
+			<?php endwhile; ?>
+		</section>
+	<?php endif; ?>
+
+	<div class="lg:flex lg:justify-between lg:items-center py-10">
 
 		<div class="lg:flex lg:items-center">
 
-
-
-
 			<div class="menu-item">&copy; <?php echo date_i18n('Y'); ?> - <?php echo get_bloginfo('name'); ?></div>
-
-
 
 			<?php
 			wp_nav_menu(
