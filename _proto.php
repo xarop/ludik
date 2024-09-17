68620c18062cfb41ab848a8658283f1e09e1e214
xarop <ajl@xarop.com>
2024-09-17 14:23:01 +0200

    proto

---
+++ _proto.php
@@ -0,0 +1,14 @@
+<?php
+/* Template Name: Proto */
+?>
+<html>
+
+<head>
+    <title>Proto</title>
+</head>
+
+<body style=" background-color: rgba(0, 0, 0, 0.15); margin:0; padding:0; overflow: hidden;">
+    <iframe style="border: 1px solid rgba(0, 0, 0, 0.1);" width="100%" height="100%" src="https://embed.figma.com/proto/cMmN3rnIWFPmFzYIbPTTLv/Ludik?page-id=94%3A1437&node-id=94-1438&node-type=frame&viewport=550%2C441%2C0.13&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=94%3A1438&embed-host=share" allowfullscreen></iframe>
+</body>
+
+</html>
